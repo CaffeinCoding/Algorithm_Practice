1b665d5b1d58a6fd6c7683c6eb322b79c92b01aa
황진태 <jintea519@gmail.com>
2022-08-23 02:30:45 +0900

    Feat BOJ 1697

--- 0819/Main_1697_숨바꼭질.java
+++ 0819/Main_1697_숨바꼭질.java
@@ -1,44 +1,57 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main_1697_숨바꼭질 {
-    static int min = Integer.MAX_VALUE;
+    static boolean flag = false;
+    static int cnt;
     static int N;
     static int M;
+
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(in.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        if(M < N){
-            min = N - M;
-        }else{
-            count(N,0, 0);
-        }
-        System.out.println(min);
-    }
-
-    public static void count(int num, int cnt, int flag){
-        if(num > 2*M || num < 0 || cnt > min){
-            return;
-        }
-        if(num == M){
-            if(cnt < min){
-                min = cnt;
+        if (M != N) {
+            if (M < N) {
+                cnt = N - M;
+            } else {
+                ArrayDeque<Integer> dq = new ArrayDeque<Integer>();
+                dq.add(N);
+                count(dq, new boolean[M * 2]);
             }
-            return;
         }
-        if(flag != 2){
-            count(num-1, cnt+1, 1);
+        System.out.println(cnt);
+    }
+
+    public static void count(ArrayDeque<Integer> dq, boolean[] visited) {
+        ArrayDeque<Integer> tdq = new ArrayDeque<Integer>();
+        cnt += 1;
+        while (!dq.isEmpty()) {
+            int tmp = dq.poll();
+            check(tdq, tmp * 2, visited);
+            check(tdq, tmp + 1, visited);
+            check(tdq, tmp - 1, visited);
+            if (flag) {
+                return;
+            }
         }
-        if(flag != 1){
-            count(num+1, cnt+1, 2);
-        }
-        if(num != 2){
-            count(2*num, cnt+1, 0);
+        if (!tdq.isEmpty()) {
+            count(tdq, visited);
         }
     }
 
+    public static void check(ArrayDeque<Integer> dq, int num, boolean[] visited) {
+        if (num == M) {
+            flag = true;
+            return;
+        }
+        if (num >= 0 && num < M * 2 && !visited[num]) {
+            dq.add(num);
+            visited[num] = true;
+        }
+    }
 }
